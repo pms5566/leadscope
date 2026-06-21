3c16fc07c963a2706dc0ac5e8ae3466534db62e7
Parmeet Singh <parmeetsingh@Parmeets-Mac-mini.local>
2026-06-21 10:48:08 +0530

    feat: add Fiverr and WhatsApp brand icons to proposal banner, update WhatsApp number

--- server.js
+++ server.js
@@ -508,7 +508,7 @@ app.get('/preview/:niche/:leadId', async (req, res) => {
     html = html.replace(/\{\{ADDRESS\}\}/g, address);
     
     // 4. Inject Sticky Top Banner & Analytics heart-beat script
-    const whatsappPhone = process.env.AGENCY_WHATSAPP_PHONE || '919999999999';
+    const whatsappPhone = process.env.AGENCY_WHATSAPP_PHONE || '917696507509';
     const fiverrUrl = process.env.AGENCY_FIVERR_URL || 'https://www.fiverr.com/s/gDeZRvL';
     const waText = encodeURIComponent(`Hi! I am looking at the custom website proposal for my business, "${businessName}". I would like to request some custom modifications!`);
     const waLink = `https://wa.me/${whatsappPhone}?text=${waText}`;
@@ -589,10 +589,14 @@ app.get('/preview/:niche/:leadId', async (req, res) => {
         </div>
         <div class="ls-banner-ctas">
           <a href="${fiverrUrl}" target="_blank" class="ls-banner-btn ls-btn-fiv" id="ls-fiverr-lnk">
-            🟢 Secure Order on Fiverr
+            <svg viewBox="0 0 24 24" style="width: 14px; height: 14px; fill: currentColor; display: inline-block; vertical-align: middle; margin-right: 6px;" xmlns="http://www.w3.org/2000/svg">
+              <path d="M23.004 15.588a.995.995 0 1 0 .002-1.99.995.995 0 0 0-.002 1.99zm-.996-3.705h-.85c-.546 0-.84.41-.84 1.092v2.466h-1.61v-3.558h-.684c-.547 0-.84.41-.84 1.092v2.466h-1.61v-4.874h1.61v.74c.264-.574.626-.74 1.163-.74h1.972v.74c.264-.574.625-.74 1.162-.74h.527v1.316zm-6.786 1.501h-3.359c.088.546.43.858 1.006.858.43 0 .732-.175.83-.487l1.425.4c-.351.848-1.22 1.364-2.255 1.364-1.748 0-2.549-1.355-2.549-2.515 0-1.14.703-2.505 2.45-2.505 1.856 0 2.471 1.384 2.471 2.408 0 .224-.01.37-.02.477zm-1.562-.945c-.04-.42-.342-.81-.889-.81-.508 0-.81.225-.908.81h1.797zM7.508 15.44h1.416l1.767-4.874h-1.62l-.86 2.837-.878-2.837H5.72l1.787 4.874zm-6.6 0H2.51v-3.558h1.524v3.558h1.591v-4.874H2.51v-.302c0-.332.235-.536.606-.536h.918V8.412H2.85c-1.162 0-1.943.712-1.943 1.755v.4H0v1.316h.908v3.558z"/>
+            </svg>
+            Secure Order on Fiverr
           </a>
           <a href="${waLink}" target="_blank" class="ls-banner-btn ls-btn-wa" id="ls-whatsapp-lnk">
-            💬 Custom Modifications
+            <i class="fa-brands fa-whatsapp" style="font-size: 14px; vertical-align: middle; margin-right: 6px;"></i>
+            Custom Modifications
           </a>
         </div>
       </div>
